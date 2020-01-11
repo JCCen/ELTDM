ec44980a74b81d52a6bd9a8adeb6e11cf19c7110
JCCen <jaimecostacentena@gmail..com>
2020-01-11 22:32:25 +0100

    removed useless files

--- code/mesure_perf.py
+++
@@ -1,4 +0,0 @@
-import timeit
-num_terations = 500
-t = timeit.Timer(stmt="kcore_function.naive_core_dec(kcore_function.g, False)", setup="import kcore_function").timeit(number=num_terations)
-print(t)
