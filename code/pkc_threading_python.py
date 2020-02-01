21424c410e26e65a2d03c638ea48c5aeb2ff27c1
avouacr <avouacr@gmail.com>
2020-02-01 13:08:11 +0100

    Debugging : on large graphs, we get marginal errors of decomp

--- code/pkc_threading_python.py
+++ code/pkc_threading_python.py
@@ -28,18 +28,18 @@ class Counters():
         with self.lock:
             self.visited += value
 
-    def reinitialize(self):
+    def reinitialize(self, G):
         self.visited = 0
         self.deg = dict(G.degree)
 
 
-def process_node(nodes_ind, G, counters, level):
+def process_nodes(nodes_ind, G, counters, level):
     """Process a sequence of nodes on a local thread."""
-    buff = np.zeros_like(nodes_ind)
+    nodes_list = list(G.nodes)
+    buff = np.zeros_like(nodes_list).astype(int)
+    # buff = np.zeros(len(nodes_ind)+1)
     start = 0
     end = 0
-
-    nodes_list = list(G.nodes)
 
     for i in nodes_ind:
         deg_i = counters.get_degree(nodes_list[i])
@@ -67,6 +67,7 @@ def pkc(G, n_threads):
     """Perform multi-threaded K-core decomposition."""
     G = G.copy()
     n = len(G.nodes)
+    assert(n_threads < n)
     counters = Counters(G)
 
     nodes_split = np.array_split(range(n), n_threads)
@@ -76,7 +77,7 @@ def pkc(G, n_threads):
 
     while visited < n:
         for t in range(n_threads):
-            thread = Thread(target=process_node, args=(nodes_split[t],
+            thread = Thread(target=process_nodes, args=(nodes_split[t],
                                                        G, counters, level))
             thread_list.append(thread)
             thread_list[t].start()
@@ -87,15 +88,31 @@ def pkc(G, n_threads):
         level += 1
         visited = counters.get_visited()
 
-    return counters.deg
+    deg = counters.deg.copy()
+    counters.reinitialize(G)
+
+    return deg
 
 
 
 # Test on toy graph
-G = nx.Graph([])
-G.add_edges_from([(1,2), (1,8), (1,9), (5,2), (5,3), (5,4), (5,6), (6,7), (6,8),
-                  (6,9), (7,8), (7,9), (8,9), (9,10), (10,11), (11,14), (11,15),
-                  (11,12), (14,15), (15,12), (12,14)])
+# G_toy = nx.Graph([])
+# G_toy.add_edges_from([(1,2), (1,8), (1,9), (5,2), (5,3), (5,4), (5,6), (6,7), (6,8),
+#                   (6,9), (7,8), (7,9), (8,9), (9,10), (10,11), (11,14), (11,15),
+#                   (11,12), (14,15), (15,12), (12,14)])
+#
+# kcore_G_toy = pkc(G_toy, 7)
+# assert(nx.core_number(G_toy) == kcore_G_toy)
 
-kcore_G = pkc(G, 5)
-assert(nx.core_number(G) == kcore_G)
+
+# Test on bigger graph
+G_big = nx.duplication_divergence_graph(100, 0.5)
+kcore_G_big_true = nx.core_number(G_big)
+kcore_G_big_pkc = pkc(G_big, 5)
+# assert(kcore_G_big_true == kcore_G_big_pkc)
+
+kcore_true = np.array(list(kcore_G_big_true.values()))
+kcore_false = np.array(list(kcore_G_big_pkc.values()))
+diff = kcore_true - kcore_false
+print(diff.sum())
+print(diff.max())
