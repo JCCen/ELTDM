5a30e4fcb4ba6efd4509f2b536988594e8c8f92b
avouacr <avouacr@gmail.com>
2020-02-01 10:12:38 +0100

    Encapsulate multithreading in functions

--- code/pkc_threading_python.py
+++ code/pkc_threading_python.py
@@ -2,10 +2,10 @@
 import networkx as nx
 import numpy as np
 from threading import Thread, Lock
-import logging
 
 
 class Counters():
+    """Encapsulate counters in a class with threadlocks to avoid race conditions."""
 
     def __init__(self, G):
         self.deg = dict(G.degree)
@@ -28,9 +28,13 @@ class Counters():
         with self.lock:
             self.visited += value
 
+    def reinitialize(self):
+        self.visited = 0
+        self.deg = dict(G.degree)
+
 
 def process_node(nodes_ind, G, counters, level):
-
+    """Process a sequence of nodes on a local thread."""
     buff = np.zeros_like(nodes_ind)
     start = 0
     end = 0
@@ -59,35 +63,39 @@ def process_node(nodes_ind, G, counters, level):
     counters.increment_visited(end)
 
 
+def pkc(G, n_threads):
+    """Perform multi-threaded K-core decomposition."""
+    G = G.copy()
+    n = len(G.nodes)
+    counters = Counters(G)
 
-# Toy graph
+    nodes_split = np.array_split(range(n), n_threads)
+    level = 0
+    visited = 0
+    thread_list = []
+
+    while visited < n:
+        for t in range(n_threads):
+            thread = Thread(target=process_node, args=(nodes_split[t],
+                                                       G, counters, level))
+            thread_list.append(thread)
+            thread_list[t].start()
+        for thread in thread_list:
+            thread.join()
+        thread_list = []
+
+        level += 1
+        visited = counters.get_visited()
+
+    return counters.deg
+
+
+
+# Test on toy graph
 G = nx.Graph([])
 G.add_edges_from([(1,2), (1,8), (1,9), (5,2), (5,3), (5,4), (5,6), (6,7), (6,8),
                   (6,9), (7,8), (7,9), (8,9), (9,10), (10,11), (11,14), (11,15),
                   (11,12), (14,15), (15,12), (12,14)])
-n = len(G.nodes)
 
-
-
-# Multithreaded PKC
-counters = Counters(G)
-n_threads = 7
-nodes_split = np.array_split(range(n), n_threads)
-
-visited = counters.get_visited()
-level = 0
-thread_list = []
-
-while visited < n:
-    for t in range(n_threads):
-        thread = Thread(target=process_node, args=(nodes_split[t], G, counters, level))
-        thread_list.append(thread)
-        thread_list[t].start()
-    for thread in thread_list:
-        thread.join()
-    thread_list = []
-
-    level += 1
-    visited = counters.get_visited()
-
-assert(nx.core_number(G) == counters.deg)
+kcore_G = pkc(G, 5)
+assert(nx.core_number(G) == kcore_G)
